52c83406d042c046a54737db3f411ddebf1b7223
SetuSingh <90468474+SetuSingh@users.noreply.github.com>
2021-10-26 16:58:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -55,79 +55,166 @@ intervalHolder = setInterval(type,200)
 window.onscroll = function(){
     var a = "col2"
     var b = "sd"
-
     console.log(scrollY)
-    if(scrollY>364){
-        document.getElementById("bruh").classList.remove('bruh')
-        document.getElementById("bruh").classList.add('bruh_red')
-        console.log( document.getElementById("bruh").classList)
 
-        // document.getElementById("bruh").classList.add("bruh_red");
-        // console.log(document.getElementById("bruh").classList)
-    }
-    if(scrollY>1148){
-        document.getElementById("bruh").classList.remove('bruh_red')
-        document.getElementById("bruh").classList.add('bruh_black')
+    if (window.innerWidth > 480){
+        if(scrollY>364){
+            document.getElementById("bruh").classList.remove('bruh')
+            document.getElementById("bruh").classList.add('bruh_red')
+            console.log( document.getElementById("bruh").classList)
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if(scrollY>1148){
+            document.getElementById("bruh").classList.remove('bruh_red')
+            document.getElementById("bruh").classList.add('bruh_black')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if(scrollY<1148 && scrollY > 364){
+            document.getElementById("bruh").classList.remove('bruh_black')
+            document.getElementById("bruh").classList.add('bruh_red')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if( scrollY < 364){
+            document.getElementById("bruh").classList.remove('bruh_black')
+            document.getElementById("bruh").classList.remove('bruh_red')
+            document.getElementById("bruh").classList.add('bruh')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+    
+    
+        if (window.innerWidth > 481 && scrollY>633){
+            a = b
+            document.getElementById("top2").classList.replace(a,"slideUp")
+            
+        }
+        if (window.innerWidth > 481 && scrollY>380){
+            a = b
+            document.getElementById("top3").classList.replace(a,"slideUp")
+        }
+    
+        if (window.innerWidth > 481 && scrollY>2130){
+            a = b
+            document.getElementById("top4").classList.replace(a,"slideUp")
+        }
+        if (window.innerWidth > 481 && scrollY<628){
+            
+            document.getElementById("top2").classList.remove("slideUP")
+            document.getElementById("top2").classList.remove(a)
+            document.getElementById("top2").classList.add(b)
+            
+    
+            a = b
+        }
+        if (window.innerWidth > 481 &&  scrollY<1345){
+            
+            document.getElementById("top3").classList.remove("slideUP")
+            document.getElementById("top3").classList.remove(a)
+            document.getElementById("top3").classList.add(b)
+            
+    
+            a = b
+        }
+        if (window.innerWidth > 481 &&  scrollY<2125){
+            
+            document.getElementById("top4").classList.remove("slideUP")
+            document.getElementById("top4").classList.remove(a)
+            document.getElementById("top4").classList.add(b)
+            
+    
+            a = b
+        }
 
-        // document.getElementById("bruh").classList.add("bruh_red");
-        // console.log(document.getElementById("bruh").classList)
     }
-    if(scrollY<1148 && scrollY > 364){
-        document.getElementById("bruh").classList.remove('bruh_black')
-        document.getElementById("bruh").classList.add('bruh_red')
-
-        // document.getElementById("bruh").classList.add("bruh_red");
-        // console.log(document.getElementById("bruh").classList)
+    if(window.innerWidth < 481){
+        if(scrollY>364){
+            document.getElementById("bruh").classList.remove('bruh')
+            document.getElementById("bruh").classList.add('bruh_red')
+            console.log( document.getElementById("bruh").classList)
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if(scrollY>1148){
+            document.getElementById("bruh").classList.remove('bruh_red')
+            document.getElementById("bruh").classList.add('bruh_black')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if(scrollY<1148 && scrollY > 364){
+            document.getElementById("bruh").classList.remove('bruh_black')
+            document.getElementById("bruh").classList.add('bruh_red')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+        if( scrollY < 364){
+            document.getElementById("bruh").classList.remove('bruh_black')
+            document.getElementById("bruh").classList.remove('bruh_red')
+            document.getElementById("bruh").classList.add('bruh')
+    
+            // document.getElementById("bruh").classList.add("bruh_red");
+            // console.log(document.getElementById("bruh").classList)
+        }
+    
+    
+        if (window.innerWidth < 481 &&  scrollY>333){
+            a = b
+            document.getElementById("top2").classList.replace(a,"slideUp")
+            
+        }
+        if (window.innerWidth < 481 && scrollY>830){
+            a = b
+            document.getElementById("top3").classList.replace(a,"slideUp")
+        }
+    
+        if (window.innerWidth < 481 && scrollY>1234){
+            a = b
+            document.getElementById("top4").classList.replace(a,"slideUp")
+        }
+        if (window.innerWidth < 481 &&  scrollY < 333){
+            
+            document.getElementById("top2").classList.remove("slideUP")
+            document.getElementById("top2").classList.remove(a)
+            document.getElementById("top2").classList.add(b)
+            
+    
+            a = b
+        }
+        if (window.innerWidth < 481 && scrollY < 830){
+            
+            document.getElementById("top3").classList.remove("slideUP")
+            document.getElementById("top3").classList.remove(a)
+            document.getElementById("top3").classList.add(b)
+            
+    
+            a = b
+        }
+        if (window.innerWidth < 481 && scrollY < 1234){
+            
+            document.getElementById("top4").classList.remove("slideUP")
+            document.getElementById("top4").classList.remove(a)
+            document.getElementById("top4").classList.add(b)
+            
+    
+            a = b
+        }
     }
-    if( scrollY < 364){
-        document.getElementById("bruh").classList.remove('bruh_black')
-        document.getElementById("bruh").classList.remove('bruh_red')
-        document.getElementById("bruh").classList.add('bruh')
-
-        // document.getElementById("bruh").classList.add("bruh_red");
-        // console.log(document.getElementById("bruh").classList)
-    }
-
-
-    if (scrollY>633){
-        a = b
-        document.getElementById("top2").classList.replace(a,"slideUp")
-        
-    }
-    if (scrollY>1365){
-        a = b
-        document.getElementById("top3").classList.replace(a,"slideUp")
-    }
-
-    if (scrollY>2130){
-        a = b
-        document.getElementById("top4").classList.replace(a,"slideUp")
-    }
-    if (scrollY<628){
-        
-        document.getElementById("top2").classList.remove("slideUP")
-        document.getElementById("top2").classList.remove(a)
-        document.getElementById("top2").classList.add(b)
-        
-
-        a = b
-    }
-    if (scrollY<1345){
-        
-        document.getElementById("top3").classList.remove("slideUP")
-        document.getElementById("top3").classList.remove(a)
-        document.getElementById("top3").classList.add(b)
-        
-
-        a = b
-    }
-    if (scrollY<2125){
-        
-        document.getElementById("top4").classList.remove("slideUP")
-        document.getElementById("top4").classList.remove(a)
-        document.getElementById("top4").classList.add(b)
-        
-
-        a = b
-    }
+    
 }
+
+// window.onscroll = function(){
+//     var a = "col2"
+//     var b = "sd"
+
+//     console.log(scrollY)
+    
+// }
